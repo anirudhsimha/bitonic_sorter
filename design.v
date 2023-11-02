cfcdba37b771c04143ffd1b44895b5797c5c1df4
anirudhsimha <105874241+anirudhsimha@users.noreply.github.com>
2023-11-02 15:40:53 +0530

    final_design.v

--- design.v
+++ design.v
@@ -1,29 +1,27 @@
-module bitonic_sorter(output wire [31:0]o1,o2, input wire [31:0]A, B, input wire clk, rst, dir, enable);
-reg sel;
-reg [31:0]out1, out2;
-always @ (posedge clk, posedge rst)
-begin
-  if (rst)
-  begin
-    outl<= 32'b0;
-    out2<= 32'b0;
-  end
-  else if (enable)
-  begin
-  if (dir == 1'b0)
-    begin
-      sel A > B;
-      outl<=(sel)?B:A;
-      out2<=(sel)?A:B;
+module bitonic_sorter(output wire [31:0] o1, o2, input wire [31:0] A, B, input wire clk, rst, dir, enable);
+  reg sel;
+  reg [31:0] out1, out2;
+
+  always @(posedge clk or posedge rst) begin
+    if (rst) begin
+      out1 <= 32'b0;
+      out2 <= 32'b0;
     end
-    if (dir == 1'b1)
-    begin
-      sel A < B;
-      out1<=(sel)?B:A;
-      out2<=(sel)?A:B;
+    else if (enable) begin
+      if (dir == 1'b0) begin
+        sel <= (A > B);
+        out1 <= (sel) ? B : A;
+        out2 <= (sel) ? A : B;
+      end
+      if (dir == 1'b1) begin
+        sel <= (A < B);
+        out1 <= (sel) ? B : A;
+        out2 <= (sel) ? A : B;
+      end
     end
   end
-end
-assign o1 = out1;
-assign o2 = out2;
+
+  assign o1 = out1;
+  assign o2 = out2;
 endmodule
+
